dd271835deb613c7ac1869e08e1c769835d17b02
shlok923 <142206161+shlok923@users.noreply.github.com>
2023-11-13 11:44:30 +0530

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,171 @@
+#----------QUESTION1----------
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    
+    def getsize(self):
+        return len(self.stack)
+    
+    def push(self,element):
+        self.stack.append(element)
+        return
+    
+    def pop(self):
+        if self.isempty() == False:
+            self.stack.pop()
+        else:
+            return "No elements to pop"
+        return
+    
+    def top(self):
+        if self.isempty() == False:
+            return self.stack[-1]
+        else:
+            return "No elements to pop"
+        return
+    
+    def isempty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+    
+    def display(self):
+        if self.isempty() == False:
+            return self.stack[::-1]
+        else:
+            return "No elements to pop"
+        return
+'''
+santosh = Stack()
+santosh.push(8)
+santosh.push(2)
+santosh.push(9)
+santosh.pop()
+santosh.push(1)
+print(santosh.getsize())
+print(santosh.top())
+print(santosh.isempty())
+print(santosh.display())
+'''
+
+#----------QUESTION2----------
+
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+    def getVal(self):
+        return self.value
+
+class LinkedList(Node):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+    
+    def append(self,data):
+        dataNode = Node(data)
+        if self.head is None:
+            self.head = dataNode
+            self.tail = dataNode
+        else:
+            self.tail.next = dataNode 
+            self.tail = self.tail.next
+        self.count += 1
+
+    def prepend(self,data):
+        data = Node(data)
+        if self.head is None:
+            self.head = data
+        else:
+            data.next = self.head
+            self.head = data
+        self.count += 1
+
+    def length(self):
+        return self.count
+
+    def isempty(self):
+        if self.count == 0:
+            return True
+        else:
+            return False
+
+    def display(self):
+        pointer = self.head
+        if self.isempty() == True:
+            return "Empty"
+        else:
+            while pointer is not None:
+                print(pointer.value)
+                pointer = pointer.next
+
+    def search(self, item):
+        pointer = self.head
+        pos = 1
+        if self.isempty() == False:
+            while pointer is not None:
+                if pointer.value == item:
+                    found = "Element found at position: " + str(pos)
+                    return found
+                pos += 1
+        return "No such element found"
+
+
+l = LinkedList()
+l.append(1)
+l.append(3)
+l.append(5)
+l.prepend(8)
+l.display()
+print(l.search(8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week5.py
@@ -0,0 +1,216 @@
+#----------QUESTION1----------
+from week4 import Stack
+from week4 import LinkedList
+from week4 import Node
+
+
+def PostFix(exp):
+    stack = Stack()
+    operations = ['+', '-', '*', '/']
+    op1 = None
+    op2 = None
+    #operation = None
+    for i in range(len(exp)):
+        if exp[i] not in operations:
+            stack.push(exp[i])
+        else:
+            op1 = int(stack.pop())
+            op2 = int(stack.pop())
+            if exp[i] == '+':
+                stack.push(op1 + op2)
+            elif exp[i] =='*':
+                stack.push(op1 * op2)
+            elif exp[i] == '-':
+                stack.push(op1 - op2)
+            elif exp[i] == '/':
+                stack.push(op1 / op2)
+    return stack.top()
+#print(PostFix('28+41+*'))
+
+
+#----------QUESTION2----------
+
+
+class BinarySearchTree():
+    def __init__(self, value, parent = None):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = parent
+    def addchild(self, child):
+        child = BinarySearchTree(child, self)
+        if self.value >= child.value:
+            if self.left == None:
+                self.left = child
+            elif self.right == None and child.value >= self.left.value:
+                self.right = child
+            else:
+                print("Left child greater than right; and only two children allowed")
+        else:
+            print("Child cannot be greater than parent")
+        return
+    #def height(self):
+
+
+    def printree(self):
+        print(self.value, self.left.value, self.right.value)
+        return
+#tree = BinarySearchTree(10)
+#tree.addchild(4)
+#tree.addchild(9)
+#tree.printree()
+
+
+class BinaryTree():
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def addLeft(self, val):
+        if self.value:
+            if self.left == None:
+                self.left = BinaryTree(val)
+            else:
+                print("value already there")
+        return
+
+    def addRight(self, val):
+        if self.value:
+            if self.right == None:
+                self.right = BinaryTree(val)
+            else:
+                print("value already there")
+        return
+
+    def InorderDisplay(self):
+        out = []
+        if self.left:
+            self.left.InorderDisplay()
+        out.append(self.value)
+        if self.right:
+            self.right.InorderDisplay()
+        return out
+
+    def Height(self):
+        if self.left == None and self.right == None:
+            return 0
+        else:
+            return (max(self.left.Height(), self.right.Height())+1)
+
+    def isBST(self):
+        inorder = self.InorderDisplay()
+        if sorted(inorder) == inorder:
+            return True
+        else:
+            return False
+
+
+
+root = BinaryTree(100)
+root.addLeft(20)
+root.addRight(50)
+root.left.addLeft(10)
+root.right.addRight(6)
+root.InorderDisplay()
+print(root.Height())
+print(root.isBST())
+
+
+#----------QUESTION3----------
+
+class Song():
+    def __init__(self, title, year, duration: float):
+        self.title = title
+        self.year = year
+        self.duration = duration
+        self.next = None
+
+class Playlist():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def appendSong(self, title, year, duration):
+        songNode = Song(title, year, duration)
+        if self.head is None:
+            self.head = songNode
+            self.tail = songNode
+        else:
+            self.tail.next = songNode 
+            self.tail = self.tail.next
+        self.count += 1
+
+    def prependSong(self, title, year, duration):
+        songNode = Song(title, year, duration)
+        if self.head is None:
+            self.head = songNode
+        else:
+            data.next = self.head
+            self.head = data
+        self.count += 1
+
+    def length(self):
+        return self.count
+
+    def isempty(self):
+        if self.count == 0:
+            return True
+        else:
+            return False
+
+    def printSongs(self):
+        pointer = self.head
+        if self.isempty() == True:
+            return "Empty"
+        else:
+            while pointer is not None:
+                print(pointer.title)
+                pointer = pointer.next
+
+    def search(self, name):
+        pointer = self.head
+        pos = 1
+        if self.isempty() == False:
+            while pointer is not None:
+                if pointer.title == name:
+                    found = "Element found at position: " + str(pos)
+                    return found
+                pos += 1
+        return "No such element found"
+#chill = Playlist()
+#chill.appendSong("Winds", 2022, 3)
+#chill.appendSong("Dance of the Corpses", 2023, 2)
+#chill.appendSong("Pills on my mind", 2022, 3)
+#chill.printSongs()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
